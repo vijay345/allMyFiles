3321edf83496044cceaf2022aa7861ab8d63c875
vijay345 <vijaysrinivasan97@gmail.com>
2020-06-07 16:07:01 +0530

    deletion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 import { FormlyModule } from '@ngx-formly/core';
-import { HomeComponent } from './home/home.component';
-import { FormlyFieldCustomInputComponent } from './formly-field-custom-input/formly-field-custom-input.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    FormlyFieldCustomInputComponent
   ],
   imports: [
     BrowserModule,
